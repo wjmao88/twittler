9196ad1f637992a8575111f5bff5c74adb23ebab
wjmao88 <wjmao88@gmail.com>
2013-12-07 21:51:51 +0800

    overall improvements. added name and renaming, sending tweets. more readable time stamp

--- data_generator.js
+++ data_generator.js
@@ -16,6 +16,9 @@ window.users = Object.keys(streams.users);
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
   var username = newTweet.user;
+  if (streams.users[username] == undefined){
+    streams.users[username] = [];
+  }
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
@@ -65,5 +68,6 @@ var writeTweet = function(message){
   var tweet = {};
   tweet.user = visitor;
   tweet.message = message;
+   tweet.created_at = new Date();
   addTweet(tweet);
 };
