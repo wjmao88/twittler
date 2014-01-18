528f3b44e96fc4452e98b7445f90b8fd138b83e7
wjmao88 <wjmao88@gmail.com>
2014-01-18 11:22:29 -0500

    debugged tabs

--- js/tabs.js
+++ js/tabs.js
@@ -4,53 +4,57 @@ Tabs.factory = function(scope){
   var $tabs = $(
     '<div class="tabs">' + 
       '<div class="tabMenu">' + 
-        '<a href=""><div class="select selected" type="home">Home</div></a>' +  
       '</div>' + 
       '<div class="tabHolder">' + 
       '</div>' + 
     '</div>' );
 
   $tabs.children('.tabHolder').append(Board.factory(scope, 'homeType', 'home'));
-
-  //event
-  $tabs.find('.tabMenu .select').click(function(){
-    var user = $(this).prop('name');
-    $(this).addClass('selected');
-    jQuery.each($tabs.find('.tabHolder > .board'), function(i, $board){
-      $board.hide();
-    });
-
-    if (user == undefined && $(this).prop('type') == 'home'){
-      $board = $tabs.find('.board.homeType.home').show();
-    } else {
-      $board = $tabs.find('.board.timeline.' + name).show();
-    }
-  });
-
-  $tabs.find('.tabMenu .select').hover(function(){
-    $(this).addClass('hover');
-  }, function(){
-    $(this).removeClass('hover');
-  });
+  $tabs.children('.tabMenu').append(Tabs.buttonFactory('Home', 'homeType', $tabs));
 
   //listen
   scope.listen('get', function(){
+    scope.log('add new users');
     var newUsers = jQuery.map(scope.newTweets, function(tweet){
       return tweet.user;
     });
     jQuery.each(newUsers, function(i, user){
       if ($tabs.find('.board.timeline.' + user).length == 0){
-        //add new board
+        scope.log('new user: ' + user);
         var $newBoard = Board.factory(scope, 'timeline', user);
         $tabs.children('.tabHolder').append($newBoard);
         $newBoard.hide();
-        //add new button
-        $newButton = '<a href=""><div class="select" name="' + user + '">' + user + '</div></a>'
-        $tabs.children('tabMenu').append($newButton);
+        $tabs.children('.tabMenu').append(Tabs.buttonFactory(user, 'timeline', $tabs));
       }
-    })
-  })
+    });
+  });
+  //
+  return $tabs;
+};
 
-}
+Tabs.buttonFactory = function(user, type, $tabs){
+  $button = $('<div class="select button" name="' + user + '" type="' + type + '">' + user + '</div>');
 
+  $button.click(function(){
+    var user = $(this).attr('name');
+    var type = $(this).attr('type')
+    scope.log('change to ' + type + ' ' + user);
+    $(this).addClass('selected');
 
+    $tabs.find('.tabHolder > .board').hide();
+    if ($(this).attr('type') == 'homeType'){
+      scope.temp = $tabs;
+      $tabs.find('.board.homeType.home').show();
+    } else {
+      $tabs.find('.board.timeline.' + user).show();
+    }
+  });
+
+  $button.hover(function(){
+    $(this).addClass('hover');
+  }, function(){
+    $(this).removeClass('hover');
+  });
+
+  return $button;
+};
